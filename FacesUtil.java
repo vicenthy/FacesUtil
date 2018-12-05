afa3a3e0f42d341d7e919c95134914f22c960ed3
Atila Augusto <atila.sistemas@gmail.com>
2018-12-05 11:45:08 -0200

    update new version primefaces

--- FacesUtil.java
+++ FacesUtil.java
@@ -1,7 +1,18 @@
+package br.com.hypnos.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.FactoryFinder;
 import javax.faces.application.FacesMessage;
@@ -16,150 +27,278 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.primefaces.PrimeFaces;
 import org.primefaces.context.RequestContext;
 import org.primefaces.model.UploadedFile;
 
+import br.com.hypnos.config.HibernateUtil;
+import br.com.hypnos.util.cdi.MyUtil;
+
 public class FacesUtil {
-	public static void adicionarMsgSave() {
-		
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação Concluída", "Registro Salvo com sucesso!"));
+
+	public static boolean isNotPostback() {
+		return !isPostback();
 	}
+
+
+	@SuppressWarnings("resource")
+	public static void convertTXTUnixToWindows(String entrada, String saida) {
+		try {
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream(new File(saida)), "ISO-8859-1"));
+
+			new BufferedReader(new FileReader(new File(entrada))).lines().forEach(linha -> {
+				try {
+					writer.write(linha.replaceAll("\r", "") + "\r\n");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void download(String saida, String format, String contentType, String name) {
+		try {
+			ExternalContext externalContext = FacesUtil.getExternalContext();
+			externalContext.setResponseContentType(contentType);
+			externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + name + "\"");
+			externalContext.setResponseHeader("Content-Length", String.valueOf(new File(saida).length()));
+			HttpServletResponse response = (HttpServletResponse) FacesUtil.getFacesContext().getExternalContext()
+					.getResponse();
+			OutputStream out = response.getOutputStream();
+			out.write(Files.readAllBytes(Paths.get(saida)));
+			out.flush();
+			out.close();
+			FileUtils.deleteQuietly(new File(saida));
+			FacesUtil.getContext().responseComplete();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void updateComponent(String id) {
+		PrimeFaces.current().ajax().update(id);
+	}
+
+	public static void updateComponentRenderId(String id) {
+		FacesContext.getCurrentInstance().getPartialViewContext().getRenderIds().add(id);
+	}
+
+	public static void addErrorMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+	}
+
+	public static void addInfoMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
+	}
+
+	public static void addWarnMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, message, message));
+	}
+
+	public static void addFatalMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_FATAL, message, message));
+	}
+
+	public static void addMessageInDialog(String message, Severity severity) {
+		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(severity, message, message));
+	}
+
+}
+
 
 	public static boolean isPostback() {
-		return FacesContext.getCurrentInstance().isPostback();
+		return getFacesContext().isPostback();
 	}
 
+
 	public static String uploadDeArquivo(String pasta, UploadedFile uploadedFile) throws IOException {
-		System.out.println( filePath(pasta, uploadedFile.getFileName()));
-		File file = new File( filePath(pasta, uploadedFile.getFileName()) );
-		if(!file.exists()){file.mkdirs();}
-		
+		System.out.println("upload de arquivo -> " + filePath(pasta, uploadedFile.getFileName()));
+		File file = new File(filePath(pasta, uploadedFile.getFileName()));
+		if (!file.exists() && !new File(folderPath(pasta)).exists()) {
+			if (!new File(folderPath(pasta)).mkdirs()) {
+				throw new RuntimeException("Não foi possível criar o diretório solicitado!");
+			} else {
+				System.out.println("Criando arquivo...");
+				file.createNewFile();
+			}
+		}
 		FileOutputStream out = new FileOutputStream(file);
 		out.write(uploadedFile.getContents());
 		out.close();
 		return file.getPath();
 	}
-	
-	public static String filePath(String pasta, String filename) {
-		return getRealPah("") + Util.separator + "WEB-INF"	+ Util.separator + pasta + Util.separator + filename;
-	}
-	public static void adicionarMsgUpdate() {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação Concluída", "Registro Atualizado com sucesso!"));
+
+	public static String uploadDeArquivo(String pasta, UploadedFile uploadedFile, String nome) throws IOException {
+		System.out.println("upload de arquivo -> " + filePath(pasta, nome));
+		File file = new File(filePath(pasta, nome));
+		if (!file.exists() && !new File(folderPath(pasta)).exists()) {
+			if (!new File(folderPath(pasta)).mkdirs()) {
+				throw new RuntimeException("Não foi possível criar o diretório solicitado!");
+			} else {
+				System.out.println("Criando arquivo...");
+				file.createNewFile();
+			}
+		}
+		FileOutputStream out = new FileOutputStream(file);
+		out.write(uploadedFile.getContents());
+		out.close();
+		return file.getPath();
 	}
 
+	public static String folderPath(String pasta) {
+		return getRealPah("") + "WEB-INF" + MyUtil.separator + pasta;
+	}
+
+	public static String filePath(String pasta, String filename) {
+		return getRealPah("") + "WEB-INF" + MyUtil.separator + pasta + MyUtil.separator + filename;
+	}
+
+	public static void adicionarMsgSave() {
+		getFacesContext().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação Concluída", "Registro Salvo com sucesso!"));
+	
+	public static void adicionarMsgUpdate() {
+		getFacesContext().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação Concluída", "Registro Atualizado com sucesso!"));
+	}
+}
+
 	public static void adicionarMsgDelete() {
-		FacesContext.getCurrentInstance().addMessage(null,
+		getFacesContext().addMessage(null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação Concluída", "Registro Excluido com sucesso!"));
 	}
 
 	public static void adicionarMsgError(String msgError) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_ERROR, msgError, ""));
+		getFacesContext().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, msgError, ""));
 	}
 
 	public static void adicionarMsgInfo(String msgInfo) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_INFO, msgInfo, "Informação"));
+		getFacesContext().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msgInfo, "Informação"));
 	}
 
 	public static HttpServletResponse getResponse() {
-		return (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		return (HttpServletResponse) getFacesContext().getExternalContext().getResponse();
 	}
 
 	public static HttpServletRequest getRequest() {
-		return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		return (HttpServletRequest) getFacesContext().getExternalContext().getRequest();
 	}
-	
+
 	public static void msgInDialog(Severity severity, String summary, String detail) {
-		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(severity, summary, detail));
+		PrimeFaces.current().dialog().showMessageDynamic(new FacesMessage(severity, summary, detail));
 	}
-	
-	
+
+	public static void closeDialog(Object obj) {
+		PrimeFaces.current().dialog().closeDynamic(obj);
+	}
+
+	public static void showXhtmlDialog(String widgetVar) {
+		PrimeFaces.current().executeScript("PF('" + widgetVar + "').show();");
+	}
+
+	public static void openCurrentDialog(String outcome, Map<String, Object> options,
+			Map<String, List<String>> parametros) {
+		PrimeFaces.current().dialog().openDynamic(outcome, options, parametros);
+	}
+
 	public static void msgInDialogSucesso(String titulo, String msg) {
-		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, msg));
+		PrimeFaces.current().dialog().showMessageDynamic(new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, msg));
 	}
 
-	
 	public static void msgInDialogError(String titulo, String msg) {
-		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, msg));
+		PrimeFaces.current().dialog().showMessageDynamic(new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, msg));
 	}
 
-	
 	public static Object getFlash(String objeto) throws ClassCastException {
-		return FacesContext.getCurrentInstance().getExternalContext().getFlash().get(objeto);
+		return getFacesContext().getExternalContext().getFlash().get(objeto);
 	}
 
 	public static Object putFlash(String alias, Object objeto) throws ClassCastException {
-		return FacesContext.getCurrentInstance().getExternalContext().getFlash().put(alias, objeto);
+		return getFacesContext().getExternalContext().getFlash().put(alias, objeto);
 	}
 
 	public static String getParam(String param) {
-		return FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get(param);
+		return getFacesContext().getExternalContext().getRequestParameterMap().get(param);
 	}
 
 	public static String putParam(String name, String param) {
-		return FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().put(name, param);
+		return getFacesContext().getExternalContext().getRequestParameterMap().put(name, param);
 	}
 
 	public static String getBrowser() {
-		return FacesContext.getCurrentInstance().getExternalContext().getRequestHeaderMap().get("User-Agent");
+		return getFacesContext().getExternalContext().getRequestHeaderMap().get("User-Agent");
 	}
-	
-	
+
 	public static FacesContext getContext() {
-		return FacesContext.getCurrentInstance();
+		return getFacesContext();
 	}
-	
+
 	public static String getRealPah(String path) {
-		return FacesContext.getCurrentInstance().getExternalContext().getRealPath(path);
+		return getFacesContext().getExternalContext().getRealPath(path);
+	}
+
+	public static FacesContext getFacesContext() {
+		return FacesContext.getCurrentInstance();
 	}
 
 	public static ExternalContext getExternalContext() {
-		return FacesContext.getCurrentInstance().getExternalContext();
+		return getFacesContext().getExternalContext();
 	}
-	
+
 	public static String getContextPath() {
-		return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+		return getFacesContext().getExternalContext().getRequestContextPath();
 	}
-	
-	public static void redirect(String url) throws IOException{
-		FacesContext.getCurrentInstance().getExternalContext().redirect(FacesUtil.getContextPath()+url);
+
+	public static void redirect(String url) throws IOException {
+		getFacesContext().getExternalContext().redirect(FacesUtil.getContextPath() + url);
 	}
-	public static void redirectSemContext(String url) throws IOException{
-		FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+
+	public static void redirectSemContext(String url) throws IOException {
+		getFacesContext().getExternalContext().redirect(url);
 	}
 
 	public static String getPaginaAtual() {
-		return FacesContext.getCurrentInstance().getViewRoot().getViewId();
+		return getFacesContext().getViewRoot().getViewId();
 	}
 
-
-	
-	public static FacesContext getFacesContext(ServletRequest request,ServletResponse response) {
-	    FacesContext facesContext = FacesContext.getCurrentInstance();
-	    if (facesContext == null) {
-	        LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY); 
-	        Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
-	        FacesContextFactory facesContextFactory = (FacesContextFactory) FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
-	        facesContext = facesContextFactory.getFacesContext(request.getServletContext(), request, response, lifecycle);
-	        facesContext.setViewRoot(facesContext.getApplication().getViewHandler().createView(facesContext, ""));
-	        FacesContextWrapper.setCurrentInstance(facesContext);
-	    }
-	    return facesContext;
+	public static FacesContext getFacesContext(ServletRequest request, ServletResponse response) {
+		FacesContext facesContext = getFacesContext();
+		if (facesContext == null) {
+			LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder
+					.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+			Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+			FacesContextFactory facesContextFactory = (FacesContextFactory) FactoryFinder
+					.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+			facesContext = facesContextFactory.getFacesContext(request.getServletContext(), request, response,
+					lifecycle);
+			facesContext.setViewRoot(facesContext.getApplication().getViewHandler().createView(facesContext, ""));
+			FacesContextWrapper.setCurrentInstance(facesContext);
+		}
+		return facesContext;
 	}
 
-	
-	private static abstract class FacesContextWrapper extends FacesContext {
-	    protected static void setCurrentInstance(FacesContext facesContext) {
-	        FacesContext.setCurrentInstance(facesContext);
-	    }
-	    
-	    
-	    
+	public static abstract class FacesContextWrapper extends FacesContext {
+		public static void setCurrentInstance(FacesContext facesContext) {
+			FacesContext.setCurrentInstance(facesContext);
+		}
+
 	}
-	
-	
-	
+
 }
